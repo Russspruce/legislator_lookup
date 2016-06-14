3075adb7796bff756e82841cb26039b706169bab
Alex Hagge and Andrew Russo <alex@>
2016-06-14 11:56:06 -0700

    added nav bar

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('results', {path: '/results/:zip'});
   this.route('committee-route', {path: '/committee-route'});
   this.route('subcommittees', {path: '/subcommittee/:committee_id'});
+  this.route('bills');
 });
 
 export default Router;
